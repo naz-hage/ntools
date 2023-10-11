4a5842106ca042689ab746989bd8e16157154340
nhage <nhage@comcast.net>
2023-10-11 16:08:56 -0500

    Update tests to pass on any machine

--- nBackupTests/NBackupTests.cs
+++ nBackupTests/NBackupTests.cs
@@ -56,14 +56,14 @@ namespace nbackup.Tests
                 Assert.IsNotNull(backups);
                 Assert.IsNotNull(backups.BackupsList);
                 Assert.AreEqual(1, backups.BackupsList.Count);
-                Assert.AreEqual("c:\\source\\dev-ops", backups.BackupsList[0].Source);
-                Assert.AreEqual("c:\\temp\\Users\\%USERNAME%\\dev-ops", backups.BackupsList[0].Destination);
-                Assert.AreEqual("/S /V /R:5 /W:5 /MT:16 /dcopy:DAT /copy:DT", backups.BackupsList[0].BackupOptions);
+                Assert.AreEqual(".", backups.BackupsList[0].Source);
+                Assert.AreEqual("%APPDATA%\\ntools", backups.BackupsList[0].Destination);
+                Assert.AreEqual("/V /R:5 /W:5 /MT:16 /dcopy:DAT /copy:DT", backups.BackupsList[0].BackupOptions);
                 Assert.IsNotNull(backups.BackupsList[0].ExcludeFolders);
                 Assert.AreEqual(2, backups.BackupsList[0].ExcludeFolders?.Count ?? 0);
                 Assert.IsNotNull(backups.BackupsList[0].ExcludeFiles);
                 Assert.AreEqual(3, backups.BackupsList[0].ExcludeFiles?.Count ?? 0);
-                Assert.AreEqual("c:\\temp\\backup.log", backups.BackupsList[0].LogFile);
+                Assert.AreEqual("%APPDATA%\\backup.log", backups.BackupsList[0].LogFile);
             }
             catch (JsonException)
             {
