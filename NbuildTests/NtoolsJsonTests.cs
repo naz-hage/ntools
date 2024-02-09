b0fe6e4cade5fe7591aa448621aec85ef8b735ef
nhage <nhage@comcast.net>
2024-02-09 12:26:13 -0600

    Update  ValidateNtoolsJsonTest and refactor json file names

--- NbuildTests/NtoolsJsonTests.cs
+++ NbuildTests/NtoolsJsonTests.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CommandLine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nbuild;
 using NbuildTasks;
 using System.Reflection;
+using System.Text.Json;
 
 namespace NbuildTests
 {
@@ -25,6 +28,38 @@ namespace NbuildTests
             // Assert
             Console.WriteLine($"ResourcePath: {targetFileName}");
             Assert.IsTrue(File.Exists(targetFileName));
+
+            ExtractToSingleAppJsonFile(targetFileName);
+        }
+
+        private static void ExtractToSingleAppJsonFile(string targetFileName)
+        {
+            var json = File.ReadAllText(targetFileName);
+
+            if (string.IsNullOrEmpty(json))
+            {
+                throw new ArgumentNullException(nameof(json));
+            }
+
+            var listAppData = JsonSerializer.Deserialize<NbuildApps>(json) ?? throw new ParserException("Failed to parse json to list of objects", null);
+
+            // make sure version matches 1.2.0
+            if (listAppData.Version != "1.2.0")
+            {
+                throw new ParserException($"Version {listAppData.Version} is not supported. Please use version 1.2.0", null);
+            }
+            // create a new json file for each item in the json file and save it to the c:\temp folder
+            foreach (var appData in listAppData.NbuildAppList)
+            {
+                var appsOne = new NbuildApps("1.2.0", [appData]);
+
+                var jsonItem = JsonSerializer.Serialize(appsOne, new JsonSerializerOptions { WriteIndented = true });
+                var jsonFilename = $"app-{appData.Name}.json";
+                jsonFilename = jsonFilename.Replace(" ", "_");
+                var fileName = Path.Combine(@"c:\temp\apps", jsonFilename);
+                File.WriteAllText(fileName, jsonItem);
+                Console.WriteLine($"File: {fileName}");
+            }
         }
     }
 }
