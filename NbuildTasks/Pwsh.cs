5519196595b822e67bbc93b87f8f2373e7d94a74
naz-hage <nhage@comcast.net>
2024-09-02 19:31:10 -0500

    Add Pwsh MSBuild task for PowerShell script execution
    Introduce a new MSBuild task named `Pwsh` in `common.targets` and `nbuild.targets` to execute PowerShell scripts using `pwsh` (PowerShell Core).
    
    - Added `Pwsh` task definition in `Pwsh.cs` with properties for `ScriptPath`, `Arguments`, and `WorkingDirectory`.
    - Updated `common.targets` to include the `Pwsh` task in the list of using tasks.
    - Added a comment in `common.targets` to standardize environment names.
    - Modified `DEV_SETUP` target in `nbuild.targets` to use the new `Pwsh` task instead of the `Exec` task for running `install.ps1`.

---
+++ NbuildTasks/Pwsh.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Build.Framework;
+using System.Diagnostics;
+using System.IO;
+
+namespace NbuildTasks
+{
+    public class Pwsh : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public string ScriptPath { get; set; }
+
+        public string Arguments { get; set; }
+
+        public string WorkingDirectory { get; set; }
+
+        public override bool Execute()
+        {
+            if (!Path.IsPathRooted(ScriptPath))
+            {
+                Log.LogError($"Task - Script Path '{ScriptPath}' is not rooted");
+            }
+
+            if (!File.Exists(ScriptPath))
+            {
+                Log.LogError($"Task - Script '{ScriptPath}' does not exist");
+            }
+            else
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "pwsh",
+                        Arguments = $"-ExecutionPolicy Bypass -File \"{ScriptPath}\" {Arguments}",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        WorkingDirectory = WorkingDirectory // Set the working directory
+                    }
+                };
+
+                process.Start();
+                process.WaitForExit();
+
+                var output = process.StandardOutput.ReadToEnd();
+                var error = process.StandardError.ReadToEnd();
+
+                Log.LogMessage(MessageImportance.High, output);
+                if (process.ExitCode != 0)
+                {
+                    Log.LogError(error);
+                    return false;
+                }
+
+                return true;
+            }
+
+            return !Log.HasLoggedErrors;
+        }
+    }
+}
