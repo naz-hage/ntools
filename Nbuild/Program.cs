dfc2066d1089808ce70bfaab19c581db3df492a1
naz-hage <naz.hage@icloud.com>
2025-05-18 12:50:48 -0500

    Refactor Command class and update usage in Program.cs
    Significantly redefined the `Command` class in `Command.cs`, adding detailed XML documentation for methods `CreateRelease`, `DownloadAsset`, and `UploadAsset`. Updated `using` directives to include necessary namespaces while removing obsolete ones.
    
    In `Program.cs`, simplified method calls to `Command.CreateRelease` and `Command.DownloadAsset` by removing the namespace prefix, aligning with the new structure of the `Command` class.

--- Nbuild/Program.cs
+++ Nbuild/Program.cs
@@ -99,9 +99,9 @@ public class Program
             Cli.CommandType.git_branch => Command.DisplayGitBranch(),
             Cli.CommandType.git_clone => Command.Clone(options.Url, options.Path, options.Verbose),
             Cli.CommandType.git_deletetag => Command.DeleteTag(options.Tag),
-            Cli.CommandType.create_release => await GitHubRelease.Command.CreateRelease(options.Repo!, options.Tag!, options.Branch!, options.AssetFileName!),
-            Cli.CommandType.create_pre_release => await GitHubRelease.Command.CreateRelease(options.Repo!, options.Tag!, options.Branch!, options.AssetFileName!,true),
-            Cli.CommandType.download_release => await GitHubRelease.Command.DownloadAsset(options.Repo!, options.Tag!, options.Path!),
+            Cli.CommandType.create_release => await Command.CreateRelease(options.Repo!, options.Tag!, options.Branch!, options.AssetFileName!),
+            Cli.CommandType.create_pre_release => await Command.CreateRelease(options.Repo!, options.Tag!, options.Branch!, options.AssetFileName!,true),
+            Cli.CommandType.download_release => await Command.DownloadAsset(options.Repo!, options.Tag!, options.Path!),
             _ => ResultHelper.Fail(-1, $"Invalid Command: '{options.Command}'"),
         };
     }
