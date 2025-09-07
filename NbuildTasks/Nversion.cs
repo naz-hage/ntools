c07b5d0b095d7c04b7f2ca3ba4b217218821ea24
naz-hage <naz.hage@icloud.com>
2025-09-07 07:04:10 -0500

    Refactor Nversion class to use AssemblyType enum for better clarity and update Get method to accept AssemblyType parameter. Remove unnecessary project properties from lf.csproj. Enhance nbuild.targets with additional logging for nb install command execution.

--- NbuildTasks/Nversion.cs
+++ NbuildTasks/Nversion.cs
@@ -3,42 +3,42 @@ using System.Diagnostics;
 using System.Reflection;
 
 namespace NbuildTasks
-{
+{ 
+    public enum AssemblyType
+    {
+        Entry,
+        Executing
+    }
+
     public static class Nversion
     {
-        public const string ExecutingAssembly = "executing";
-
         /// <summary>
         /// Retrieves the file version information for the entry or executing assembly.
         /// </summary>
-        /// <param name="assembly">The assembly to retrieve the file version information from. If empty, retrieves information from the entry assembly.</param>
-        /// <returns>The formatted string containing the file description, product name, company name, legal copyright, and file version.</returns>
-        public static string Get(string assembly = "")
+        /// <param name="type">
+        /// Specifies which assembly to retrieve the file version information from.
+        /// <see cref="AssemblyType.Entry"/> gets the entry assembly; <see cref="AssemblyType.Executing"/> gets the currently executing assembly.
+        /// </param>
+        /// <returns>
+        /// The formatted string containing the file description, product name, company name, legal copyright, and file version.
+        /// </returns>
+        public static string Get(AssemblyType type = AssemblyType.Entry)
         {
+            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(
+                type == AssemblyType.Executing
+                    ? Assembly.GetExecutingAssembly().Location
+                    : Assembly.GetEntryAssembly().Location
+            );
 
-            FileVersionInfo fileVersionInfo = FileVersionInfo
-                                                .GetVersionInfo(Assembly
-                                                .GetEntryAssembly()
-                                                .Location);
-            if (assembly == ExecutingAssembly)
-            {
-                fileVersionInfo = FileVersionInfo.
-                                                GetVersionInfo(Assembly.
-                                                GetExecutingAssembly().
-                                                Location);
-
-            }
-
-            string updatedCopyright = fileVersionInfo
-                            .LegalCopyright
-                            .Replace("XXXX", DateTime.Now.Year.ToString());
+            string updatedCopyright = fileVersionInfo.LegalCopyright.Replace("XXXX", DateTime.Now.Year.ToString());
 
             return $" *** {fileVersionInfo.FileDescription}, " +
-                            $"{fileVersionInfo.ProductName}, " +
-                            $"{fileVersionInfo.CompanyName}, " +
-                            $"{updatedCopyright} - " +
-                            $" Version: {fileVersionInfo.FileVersion}";
-
+                   $"{fileVersionInfo.ProductName}, " +
+                   $"{fileVersionInfo.CompanyName}, " +
+                   $"{updatedCopyright} - " +
+                   $" Version: {fileVersionInfo.FileMajorPart}." +
+                   $"{fileVersionInfo.ProductMinorPart}." +
+                   $"{fileVersionInfo.ProductBuildPart}";
         }
     }
 }
